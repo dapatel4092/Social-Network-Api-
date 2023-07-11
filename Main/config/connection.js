7e3d5a9a6a165df7cf29b88ae109583ed2ec7bba
Dhruv <dapatel700@gmail.com>
2023-07-10 17:05:01 -0700

    changes made to routes and controllers

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -3,8 +3,7 @@ require("dotenv").config()
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://bizarrowill:Password123!@cluster0.8ogzorc.mongodb.net/';
-
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialdb'
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
